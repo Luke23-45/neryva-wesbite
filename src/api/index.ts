dd255280b7331a98a0163e1b672cf8d3796da370
krishna subedi <krishnasubedi2366@gmail.com>
2026-07-29 12:16:03 +0545

    feat(): add backend url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import { useAuthStore } from '@/store/authStore';
 
-const API_BASE_URL = import.meta.env.DEV ? '/api/v1' : import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.DEV
+  ? '/api/v1'
+  : 'https://neryva-backend.vercel.app/api/v1';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
